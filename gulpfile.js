7494d43b1c81782a45ffb715c32013660ff6f028
dkersten <dkersten1994@gmail.com>
2020-10-18 00:46:19 -0400

    gulp starter v1 complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,71 @@
-const {series} = require ('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 
-function transpile (cb) {
-  cb ();
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
+
+// file paths
+
+const files = {
+  scssPath: 'app/scss/**/*.scss',
+  jsPath: 'app/js/**/*.js'
 }
 
-function bundle (cb) {
-  cb ();
+// compile style.scss into style.css
+const scssTask = () => {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sass()) // compile scss -> css
+    .pipe(postcss([ autoprefixer(), cssnano() ])) // postcss plugins
+    .pipe(sourcemaps.write('.')) // write sourcemaps in current dir
+    .pipe(dest('dist')) // put final css in dist folder
 }
 
-exports.build = series (transpile, build);
+// concat and uglify JS files to all.js
+const jsTask = () => {
+  return src([
+    files.jsPath
+  ])
+    .pipe(concat('all.js'))
+    // .pipe(uglify())
+    .pipe(dest('dist'))
+}
+
+// cachebust
+const cbString = new Date().getTime()
+
+const cacheBustTask = () => {
+  return src(['index.html'])
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('.'))
+}
+
+// watch task: watch scss and js files for changes. If any changes run scss and js task simultaneously
+const watchTask = () => {
+  watch([files.scssPath, files.jsPath],
+      series(
+        parallel(scssTask, jsTask),
+        cacheBustTask
+      )
+    )
+}
+
+// export default gulp task so it can be run. runs the scss and js task simultaneously then runs cachebust, then watch task
+exports.default = series(
+  parallel(scssTask, jsTask),
+  cacheBustTask,
+  watchTask
+)
 
 // babel
 // scss
 // css prefixer
-// css minify
-// js minify
+// css minify [x]
+// js minify [x]
 // image minify
 // live reload
